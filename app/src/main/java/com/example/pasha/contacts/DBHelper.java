18e40f34759b4e69cca14ba10a87b418af64e17e
Pasha <hrytsiuk27@gmail.com>
2017-09-18 22:01:38 +0300

    cleaning

--- app/src/main/java/com/example/pasha/contacts/DBHelper.java
+++ app/src/main/java/com/example/pasha/contacts/DBHelper.java
@@ -5,22 +5,18 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-/**
- * Created by Pasha on 07.12.2016.
- */
 
 public class DBHelper extends SQLiteOpenHelper {
 
 
     public DBHelper(Context context) {
-        // конструктор суперкласса
         super(context, "contactDB", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         Log.d("TAG", "--- onCreate database ---");
-        // создаем таблицу с полями
+        // create table with fields
         db.execSQL("create table contacts ("
                 + "id integer primary key autoincrement," + "name text,"
                 + "surname text," + "phone text," + "other text" + ");");
